2cc538195d167009d22a3f74f8b68a49ee94f4b6
A_Simms <114841649+amnewby1@users.noreply.github.com>
2023-05-17 21:06:10 -0400

    Add files via upload

---
+++ 28.3_Exercise/models.py
@@ -0,0 +1,32 @@
+"""Models for Cupcake app."""
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+stock_image="https://tinyurl.com/demo-cupcake"
+
+def connect_db(app):
+    db.app = app
+    db.init_app(app)
+    app.app_context().push()
+
+
+class Cupcake(db.Model):
+    """Cupcake Model"""
+    __table_name="cupcakes"
+
+    id=db.Column(db.Integer, primary_key=True, autoincrement=True)
+    flavor=db.Column(db.Text, nullable=False)
+    size = db.Column(db.Text, nullable=False)
+    rating=db.Column(db.Float, nullable=False)
+    image=db.Column(db.Text, nullable=False, default=stock_image)
+
+    """Serialize a cupcake instance so we can jsonify"""
+    def serialize_cupcake(self):
+        return {
+            "flavor": self.flavor,
+            "size": self.size,
+            "rating": self.rating,
+            "image": self.image
+
+        }
